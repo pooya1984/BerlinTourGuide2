7961aba78ecfbdec988df1f5dca3113a00974e8c
pooya1984 <pooya.nodehi@gmail.com>
2017-07-08 12:31:13 +0200

    initial commit

--- app/src/main/java/com/example/android/berlintourguide/Word.java
+++ app/src/main/java/com/example/android/berlintourguide/Word.java
@@ -5,16 +5,12 @@ package com.example.android.berlintourguide;
  */
 
 public class Word {
-    /** Default translation for the Word */
     private String mNameOfPlace;
 
-    /** Miwok translation for the Word */
     private String mAdressOfPlace;
 
-    /** Image resource ID for the word */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
 
-    /** Constant value that represents no image was provided for this word */
     private static final int NO_IMAGE_PROVIDED = -1;
 
 
@@ -23,26 +19,14 @@ public class Word {
         mAdressOfPlace = adressOfPlace;
         mImageResourceId = imageResourceId;
     }
-    /**
-     * Get the name of place.
-     */
     public String getNameOfPlace() {
         return mNameOfPlace;
     }
-
-    /**
-     * Get the adress of place.
-     */
     public String getAdressOfPlace() {
         return mAdressOfPlace;
     }
-
     public int getImageResourceId(){return mImageResourceId;}
 
-
-    /**
-     * Returns whether or not there is an image for this word.
-     */
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
     }}
